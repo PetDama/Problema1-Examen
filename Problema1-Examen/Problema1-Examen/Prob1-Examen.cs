490cfa80b531ff59fd4a8929ce757a2146a5c0ad
Damaris Florina Petrila <damaris.petrila@emmanuel.ro>
2022-02-07 09:42:06 +0100

    Problema1-Examen

---
+++ Problema1-Examen/Problema1-Examen/Prob1-Examen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Test
+{
+
+    static int[] arr = { 1, 2, 3, 4, 5 };
+
+
+    static int findSum(int[] X, int Y)
+    {
+        if (Y <= 0)
+            return 0;
+        return (findSum(X, Y - 1) + X[Y - 1]);
+    }
+
+    public static void Main()
+    {
+        Console.Write(findSum(arr, arr.Length));
+    }
+}
---
+++ Problema1-Examen/Problema1-Examen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problema1_Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
